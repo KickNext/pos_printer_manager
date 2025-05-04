05c9b96afd3a2d614a863df4e980de53558ebcc5
KickNext <nikitiser@gmail.com>
2025-05-04 17:36:19 +0500

    Adds top padding to improve layout consistency
    Introduces a 16px top padding to align with existing padding
    values, enhancing the visual consistency of the layout on
    wide screens.

--- lib/src/ui/printer_detail_screen.dart
+++ lib/src/ui/printer_detail_screen.dart
@@ -67,6 +67,7 @@ class _PrinterDetailsScreenState extends State<PrinterDetailsScreen> {
                   padding:
                       isWide
                           ? const EdgeInsets.only(
+                            top: 16,
                             left: 16,
                             right: 16,
                             bottom: 16,
